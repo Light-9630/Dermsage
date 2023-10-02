2d63ecf43bf9bd06d0345f2766306819579d0f1e
Prakash <prakashlight1253@gmail.com>
2023-10-03 01:02:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,21 +8,104 @@ model = tf.keras.models.load_model("trained_model.h5")
 
 #Showing the logo
 
-with open("logo_1.png", "rb") as icon_image_file:
-    icon_image_data = icon_image_file.read()
-
 st.markdown(
-    f"""
+    """
     <style>
-    img.rounded {{
+    .rounded-image {
         border-radius: 50%;
-    }}
+        overflow: hidden;
+    }
     </style>
     """,
     unsafe_allow_html=True,
 )
 
-st.image(icon_image_data, use_column_width=True, output_format='PNG', unsafe_allow_html=True)
+# Wrap the image in a div with the rounded-image class
+st.write(
+    f'<div class="rounded-image"><img src="data:image/png;base64,{icon_image_data.decode()}" alt="Dermsage Logo" /></div>',
+    unsafe_allow_html=True
+)
+
+# Title and Description
+st.markdown("<p style='font-size: 18px; color: #333; text-align: center;'>Welcome to Dermsage! Upload an image to check for skin diseases.</p>", unsafe_allow_html=True)
+
+# Get user's name
+st.markdown("<br>", unsafe_allow_html=True)
+user_name = st.text_input("Enter your name:")
+
+# Upload Image
+uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+# Check if an image has been uploaded
+if uploaded_image is not None:
+    # Convert RGBA image to RGB
+    image = Image.open(uploaded_image).convert("RGB")
+    
+    # Resize the image to a smaller version
+    smaller_image = image.resize((150, 150))  # You can adjust the size as needed
+    
+    # Display the smaller uploaded image
+    st.image(smaller_image, caption="Uploaded Image (Smaller)", use_column_width=True)
+
+    # Resize the image to the desired dimensions for prediction
+    image_for_prediction = image.resize((224, 224))
+    
+    # Convert to NumPy array
+    image_for_prediction = np.asarray(image_for_prediction)
+    image_for_prediction = image_for_prediction / 255.0  # Normalize the image data
+    image_for_prediction = np.expand_dims(image_for_prediction, axis=0)
+
+    # Make predictions
+    prediction = model.predict(image_for_prediction)
+
+    st.success("Prediction Complete!")
+
+    class_names = [
+        "Acne",
+        "Eczema",
+        "Normal Skin",
+        "Psoriasis/Lichen Planus",
+        "Fungal Infections",
+        "Vitiligo"
+    ]
+
+    # Display the prediction
+    st.markdown("<h3 style='text-align: center; color: #0CAFFF;'>Result</h3>", unsafe_allow_html=True)
+    predicted_class_index = np.argmax(prediction)
+    predicted_class = class_names[predicted_class_index]
+    st.markdown(f"<p style='font-size: 24px; color: #0CAFFF; text-align: center;'><strong>Hi {user_name},</strong></p>", unsafe_allow_html=True)
+    st.markdown(f"<p style='font-size: 24px; color: #0CAFFF; text-align: center;'>The image you uploaded is classified as:</p>", unsafe_allow_html=True)
+    st.markdown(f"<p style='font-size: 24px; color: black; text-align: center;'><strong>{predicted_class}</strong></p>", unsafe_allow_html=True)
+
+    # Brief explanation about the predicted class
+    class_descriptions = [
+        " Acne is a common skin condition that causes pimples and other blemishes. Rosacea is a chronic skin condition that causes redness and visible blood vessels on the face.",
+        " Eczema is a condition that causes the skin to become red, itchy, and inflamed. It often appears as dry, scaly patches on the skin.",
+        " This image appears to be of normal skin with no signs of any specific skin condition.",
+        " Psoriasis is a chronic skin condition that causes cells to build up rapidly on the surface of the skin. Lichen Planus is an inflammatory skin condition. Both can cause rashes and skin lesions.",
+        " These are fungal skin infections that can cause itching, redness, and rashes on the skin.",
+        " Vitiligo is a long-term skin condition characterized by patches of the skin losing their pigment. This results in the appearance of white patches on the skin."
+    ]
+
+    # Show description of the predicted class
+    st.markdown(f"<p style='font-size: 18px; color: #333; text-align: left;'><b>Description:</b>{class_descriptions[predicted_class_index]}</p>", unsafe_allow_html=True)
+
+    # Note
+    st.markdown("<div style='font-size: 6px; border: 2px solid black; padding: 5px; margin-top: 15px;'><p><strong>Note:</strong> The AI model used in this application is under development. Please use the results as general information and consult a healthcare professional for accurate diagnosis and treatment.</p></div>", unsafe_allow_html=True)
+
+# About Dermsage
+st.sidebar.markdown("<h2>About Dermsage:</h2>", unsafe_allow_html=True)
+st.sidebar.markdown("<p>Dermsage is an AI-powered skin disease detection service. We aim to provide quick and accurate skin disease diagnosis to our users.</p>", unsafe_allow_html=True)
+
+# Benefits
+st.sidebar.markdown("<h2>Benefits:</h2>", unsafe_allow_html=True)
+This code adds a CSS class to the logo image to achieve rounded edges using HTML and CSS.
+
+
+
+
+
+
 # Title and Description
 st.markdown("<p style='font-size: 18px; color: #333; text-align: center;'>Welcome to Dermsage! Upload an image to check for skin diseases.</p>", unsafe_allow_html=True)
 
