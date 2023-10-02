8d8aa2182c8e9b6d30d3bdb51e8f7c18446b1a6e
Prakash <prakashlight1253@gmail.com>
2023-10-02 22:54:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ if uploaded_image is not None:
     # Show description of the predicted class
     st.markdown(f"<p style='font-size: 18px; color: #333; text-align: left;'><b>Description:</b>{class_descriptions[predicted_class_index]}</p>", unsafe_allow_html=True)
     # Note
-    st.markdown("<div style='font-size: 9px; border: 2px solid black; padding: 5px; margin-top: 15px;'><p><strong>Note:</strong> The AI model used in this application is under development. Please use the results as general information and consult a healthcare professional for accurate diagnosis and treatment.</p></div>", unsafe_allow_html=True)
+    st.markdown("<div style='font-size: 7px; border: 2px solid black; padding: 5px; margin-top: 15px;'><p><strong>Note:</strong> The AI model used in this application is under development. Please use the results as general information and consult a healthcare professional for accurate diagnosis and treatment.</p></div>", unsafe_allow_html=True)
 
     
 # About Dermsage
