8efae7e5b7fa32ebc8140b7cd431ee8bf7688afb
Prakash <prakashlight1253@gmail.com>
2023-10-02 23:36:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ model = tf.keras.models.load_model("trained_model.h5")
 # st.markdown(hidden, unsafe_allow_html=True)
 # Dermsage logo and Title
 
+# Set the default theme to "light"
+st.set_page_config(page_title="Dermsage App", page_icon=None, layout="wide", theme="light")
+
 # Load the Dermsage logo image
 with open("logo_streamlit.jpg", "rb") as logo_image_file:
     logo_image_data = logo_image_file.read()
