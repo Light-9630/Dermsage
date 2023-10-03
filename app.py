63abd5443645f04767103d719e17ef992d3c16d3
Prakash <prakashlight1253@gmail.com>
2023-10-04 00:27:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.sidebar.markdown("<h2 class='header'>Contact Us</h2>", unsafe_allow_html=True
 st.sidebar.markdown("<p>If you have any questions or feedback, please email us at dermsage@gmail.com.</p>", unsafe_allow_html=True)
 
 # Privacy Policy
-st.sidebar.markdown("<h2 class='header'>Privacy Policy</h2>", unsafe_allow_html=True)
+st.sidebar.markdown("<h2 class='header'>Privacy</h2>", unsafe_allow_html=True)
 st.sidebar.markdown("<ul><li>Your data is safe.</li><li>We don't save anything.</li><li>We assure your privacy.</li></ul>", unsafe_allow_html=True)
 
 # Additional CSS styles for overall appearance
